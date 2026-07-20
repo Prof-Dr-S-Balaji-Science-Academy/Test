da0e021a81fbd401aeeb7f40e41665462515a171
Prof. Dr. S. Balaji <cbsedigest@gmail.com>
2026-07-20 13:07:30 +0530

    Delete js directory

--- js/footer.js
+++
@@ -1,3 +0,0 @@
-/* FOOTER FUNCTIONALITY */
-// Footer component is primarily static. Add any footer-specific interactivity here.
-// Currently, all footer links are handled by default anchor tag behavior.
